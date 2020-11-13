1a8b2f667490623e69a0fca3481dd7e3dc973c58
Nicolle Gonzalez <nicolle@Nicolles-MBP.lan>
2020-11-13 14:10:47 -0800

    Added a button

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
-
-      <Text style={styles.instructions} >
+      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
+
+      <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -23,11 +26,21 @@ const styles = StyleSheet.create({
   logo: {
     width: 305,
     height: 159,
-    marginBottom: 10,
+    marginBottom: 20,
   },
   instructions: {
     color: '#888',
     fontSize: 18,
     marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   }, 
 });
